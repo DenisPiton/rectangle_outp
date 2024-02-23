822d8d7c7fb8e86743a61ef9c21602eeaf467834
Denis <denfill008@mail.ru>
2024-02-23 18:26:16 +0300

    Добавьте файлы проекта.

---
+++ rectangle/rectangle.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+	vector<int> v(10);
+	for (int i = 0; i <= 9; i++)
+	{
+		v[i] = (i+1)*(i+1);
+	}
+	for (int i = 0; i <= 9; i++)
+	{
+		cout << v[i] << "\t";
+	}
+}
+
